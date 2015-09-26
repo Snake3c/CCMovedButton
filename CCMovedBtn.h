e5b8ac63754fda4c5859baed119757977b2114cd
Snake3c <snake3c@163.com>
2015-09-26 13:56:59 +0800

    first

---
+++ CCMovedBtn.h
@@ -0,0 +1,12 @@
+//
+//  CCMovedBtn.h
+//
+//
+//  Created by cc on 15/9/25.
+//  Copyright © 2015年 Snake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCMovedBtn : UIButton
+@end
